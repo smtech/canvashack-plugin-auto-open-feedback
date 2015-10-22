cc0651560a2f31de778ea4355131bfc1a7637658
Seth Battis <sethbattis@stmarksschool.org>
2015-10-21 22:24:51 -0400

    Munging about
    Working towards a standard syntax for `manifest.xml` and the accompanying javascript.

--- auto-open-feedback.js
+++ auto-open-feedback.js
@@ -1,10 +1,11 @@
-function openPreview() {
-	// open the document preivew (if it's there)
-	$('#preview_frame').contents().find('.content-box').find('.col-xs-5.align-right').find('a')[0].click();
+var canvashack = {
+	openPreview: function() {
+		// open the document preivew (if it's there)
+		return $('#preview_frame').contents().find('.content-box').find('.col-xs-5.align-right').find('a')[0].click();
+	},
 	
-}
-
-function delayOpenPreview() {
-	// not sure why I need to have the second delay -- but if I don't the document downloads, rather than previews
-	$('#preview_frame').load(window.setTimeout(openPreview, 1000));
-}
+	delayOpenPreview: function() {
+		// not sure why I need to have the second delay -- but if I don't the document downloads, rather than previews
+		return $('#preview_frame').load(window.setTimeout(this.openPreview, 1000));
+	}
+};
