236d4a12173961a2e091e2bb34c461b0e5884a61
Seth Battis <sethbattis@stmarksschool.org>
2016-10-24 13:49:58 -0400

    Missing this
    ...also some JSLint massaging

--- auto-open-feedback.js
+++ auto-open-feedback.js
@@ -4,21 +4,17 @@ var canvashack = {
         try {
             // open the document preivew (if it's there)
             $('#preview_frame').contents().find('.modal_preview_link')[0].click();
-        } catch (error) {
-            // do nothing
-        }
+        } catch (ignore) {}
 
         try {
             // open the rubric, if it's there
             $('.assess_submission_link.rubric').click();
-        } catch (error) {
-            // do nothing
-        }
+        } catch (ignore) {}
     },
 
     openFeedback: function () {
         "use strict";
         // not sure why I need to have the second delay -- but if I don't the document downloads, rather than previews
-        window.setTimeout(delayOpenPreview, 1000);
+        window.setTimeout(this.delayOpenPreview, 1000);
     }
 };
